42e0a762d5087ee2b465dfe58f70ca05f010e235
Rumsha24 <rumsha26@gmail.com>
2025-06-01 10:01:59 -0400

    Test2

--- TriangleTest/Test.cs
+++ TriangleTest/Test.cs
@@ -12,7 +12,7 @@ namespace TestClass
     public class Test
     {
         [Test]
-      public void equilaterialTriangleTest()
+        public void equilaterialTriangleTest()
         {
 
             // Arrange
@@ -23,7 +23,6 @@ namespace TestClass
             string result = TriangleSolver.Triangle.AnalyzeTriangle(firstSide, secondSide, thirdSide);
             // Assert
             Assert.AreEqual("Equilateral triangle", result);
-        
 
 
 
@@ -32,6 +31,54 @@ namespace TestClass
 
 
 
+
+        }
+
+
+
+        [Test]
+        public void isoscelesTriangleTest1()
+        {
+            // Arrange
+            int firstSide = 15;
+            int secondSide = 15;
+            int thirdSide = 8;
+            // Act
+            string result = TriangleSolver.Triangle.AnalyzeTriangle(firstSide, secondSide, thirdSide);
+            // Assert
+            Assert.AreEqual("Isosceles triangle", result);
+        }
+        [Test]
+
+        public void isoscelesTriangleTest2()
+        {
+            // Arrange
+            int firstSide = 18;
+            int secondSide = 20;
+            int thirdSide = 12;
+            // Act
+            string result = TriangleSolver.Triangle.AnalyzeTriangle(firstSide, secondSide, thirdSide);
+            // Assert
+
+
+        }
+
+
+        [Test]
+
+        public void isoscelesTriangleTest3()
+        {
+            // Arrange
+            int firstSide = 22;
+            int secondSide = 20;
+            int thirdSide = 9;
+            // Act
+            string result = TriangleSolver.Triangle.AnalyzeTriangle(firstSide, secondSide, thirdSide);
+            // Assert
+
+
+        }
     }
-    }
+
+
 }
