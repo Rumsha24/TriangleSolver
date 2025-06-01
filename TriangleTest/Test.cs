f4bf73a6e0e278aaefe860e3c94ee830ed2a1788
Rumsha24 <rumsha26@gmail.com>
2025-06-01 09:51:46 -0400

    Test1

---
+++ TriangleTest/Test.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+
+namespace TestClass
+{
+    [TestFixture]
+    public class Test
+    {
+        [Test]
+      public void equilaterialTriangleTest()
+        {
+
+            // Arrange
+            int firstSide = 13;
+            int secondSide = 13;
+            int thirdSide = 13;
+            // Act
+            string result = TriangleSolver.Triangle.AnalyzeTriangle(firstSide, secondSide, thirdSide);
+            // Assert
+            Assert.AreEqual("Equilateral triangle", result);
+        
+
+
+
+
+
+
+
+
+    }
+    }
+}
